72b5993fa3d42f836b10747fbb0e984bf16845bb
sewgway <panoslouvros@gmail.com>
2023-03-20 18:25:29 +0000

    a

--- tessimpy2.py
+++ tessimpy2.py
@@ -2,8 +2,9 @@ import simpy
 import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.animation import PillowWriter
+import os 
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
-print('hi')
 worker_panel = 3
 initial_steel  = 0
 panel_capacity = 40
@@ -103,8 +104,8 @@ ylist2=[]
 expopanels=[]
 xlist=[]
 
-# env=simpy.Environment()
-env=simpy.rt.RealtimeEnvironment(factor=0.01, strict=False)
+env=simpy.Environment()
+# env=simpy.rt.RealtimeEnvironment(factor=0.01, strict=False)
 
 ImportExport_warehouse = ImportExport_warehouse(env)
 FabricationHall = FabricationHall(env)
@@ -126,8 +127,8 @@ writer = PillowWriter(fps=15, metadata=metadata)
 
 plt.xlim(0,SIMTIME)
 plt.ylim(0,100)
-
-with writer.saving(fig, 'animation.gif', 100):
+output_path = os.path.join(dir_path, 'animation.gif')
+with writer.saving(fig, output_path, 100):
     for i in range(len(xlist)):
         l.set_data(xlist[0:i], ylist1[0:i])
         m.set_data(xlist[0:i], ylist2[0:i])
